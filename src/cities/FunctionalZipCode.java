01dd7e53f6370cdb433979d10795685403ff61bf
BrandonChase <coding@brandonchase.me>
2017-11-02 23:00:09 -0500

    Working

--- src/cities/FunctionalZipCode.java
+++ src/cities/FunctionalZipCode.java
@@ -13,7 +13,6 @@ import java.util.Locale;
 public class FunctionalZipCode {
 	static final String INPUT_FILENAME = "L03a zip_code_database.csv";
 	static final String OUTPUT_FILENAME = "Problem_3a_output.txt";
-	static boolean SHOW_INFO; // Used in lambda stream.forEach() to handle logic for only showing county and city info on first zip type entry
 	
 	public static void main(String[] args) {
 		try {
@@ -38,7 +37,6 @@ public class FunctionalZipCode {
 						 		 .distinct()
 						 		 .sorted()
 						 		 .forEach(city -> { //go through each city in each county in alphabetical order
-						 			 SHOW_INFO=true;
 						 			 zipCodes.stream()
 							 		 .filter(zipCode -> zipCode.getCountyName().equals(county) && zipCode.getCityName().equals(city)) //filter to stream of zipCodes in current county and city
 							 		 .map(zipCode -> zipCode.getTypeZip()) //convert to stream of zip types
@@ -46,18 +44,26 @@ public class FunctionalZipCode {
 							 		 .sorted()
 							 		 .forEach(zipType -> {//go through each zip type in current county / city pair
 							 			try {
-							 				if(SHOW_INFO) { //boolean defaults to false
+							 				//if this is first zip type for county/city pair, write all info
+								 			if(zipCodes.stream()
+													   .filter(zipCode -> zipCode.getCountyName().equals(county) && zipCode.getCityName().equals(city))
+													   .map(zipCode -> zipCode.getTypeZip())
+													   .distinct()
+													   .sorted()
+													   .findFirst()
+													   .get().equals(zipType)
+													   ) {
 								 				//-----County Name-----
-								 				bWriter.write(county);
-								 				bWriter.write("\t");
+									 			bWriter.write(county);
+									 			bWriter.write("\t");
 												 
 												//-----City Name-----
-								 				bWriter.write(city);
-								 				bWriter.write("\t");
-							 				} else {
-							 					bWriter.write("\t");
-							 					bWriter.write("\t");
-							 				}
+									 			bWriter.write(city);
+									 			bWriter.write("\t");
+									 		} else {
+									 			bWriter.write("\t");
+									 			bWriter.write("\t");
+									 		}
 							 				
 							 				//-----Zip Type-----
 							 				bWriter.write(zipType);
@@ -86,20 +92,26 @@ public class FunctionalZipCode {
 														           		 .count()));
 											 bWriter.write("\t");
 											 
-											 if(SHOW_INFO) {//boolean defaults to false
-												 //-----County Population-----
+											//if this is first zip type for county/city pair, write all info
+											 if(zipCodes.stream()
+													   .filter(zipCode -> zipCode.getCountyName().equals(county) && zipCode.getCityName().equals(city))
+													   .map(zipCode -> zipCode.getTypeZip())
+													   .distinct()
+													   .sorted()
+													   .findFirst()
+													   .get().equals(zipType)
+													   ) {
+											     //-----County Population-----
 												 bWriter.write(NumberFormat.getNumberInstance(Locale.US).format(
-													 		zipCodes.stream()
-														            .filter(zipCode -> zipCode.getCountyName().equals(county))
-														            .mapToInt(zipCode -> zipCode.getEstPop())
-														            .sum()));
+														 		zipCodes.stream()
+															            .filter(zipCode -> zipCode.getCountyName().equals(county) && zipCode.getCityName().equals(city))
+															            .mapToInt(zipCode -> zipCode.getEstPop())
+															            .sum()));
 												 bWriter.write("\t");
-												 
-												 SHOW_INFO = false; //disable showing info for subsequent rows in same county+city combination
 											 } else {
 												 bWriter.write("\t");
 											 }
-											 
+
 											 bWriter.newLine();
 							 			 } catch(Exception e) {
 							 				System.out.println(e.getMessage());
